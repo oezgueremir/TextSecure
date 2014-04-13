f63cf004703f456f90383f202b42725937b2bab3
Özgür Emir <oezguer@3mir.de>
2014-04-14 00:59:01 +0200

    Better classification of elapsed time

--- src/org/thoughtcrime/securesms/util/DateUtils.java
+++ src/org/thoughtcrime/securesms/util/DateUtils.java
@@ -13,6 +13,10 @@ public class DateUtils extends android.text.format.DateUtils {
   private final static long WEEK_IN_MILLIS = 7 * DAY_IN_MILLIS;
   private final static long YEAR_IN_MILLIS = (long)(52.1775 * WEEK_IN_MILLIS);
 
+  private static boolean isWithin24H(final long millis) {
+    return System.currentTimeMillis() - millis <= DAY_IN_MILLIS;
+  }
+
   private static boolean isWithinWeek(final long millis) {
     return System.currentTimeMillis() - millis <= (WEEK_IN_MILLIS - DAY_IN_MILLIS);
   }
@@ -22,19 +26,18 @@ public class DateUtils extends android.text.format.DateUtils {
   }
 
   public static String getBetterRelativeTimeSpanString(final Context c, final long millis) {
-    final String prettyDate;
+    int formatFlags;
     if (isToday(millis)) {
-      prettyDate = DateUtils.formatDateTime(c, millis, DateUtils.FORMAT_SHOW_TIME | DateUtils.FORMAT_ABBREV_TIME);
+      formatFlags = DateUtils.FORMAT_SHOW_TIME | DateUtils.FORMAT_ABBREV_TIME;
+    } else if (isWithin24H(millis)) {
+      formatFlags = DateUtils.FORMAT_SHOW_TIME | DateUtils.FORMAT_ABBREV_TIME | DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_ABBREV_WEEKDAY;
     } else if (isWithinWeek(millis)) {
-      prettyDate = DateUtils.formatDateTime(c, millis,
-          DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_ABBREV_WEEKDAY);
+      formatFlags = DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_ABBREV_WEEKDAY;
     } else if (isWithinYear(millis)) {
-      prettyDate = DateUtils.formatDateTime(c, millis,
-          DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_NO_YEAR | DateUtils.FORMAT_ABBREV_ALL);
+      formatFlags = DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_NO_YEAR | DateUtils.FORMAT_ABBREV_ALL;
     } else {
-      prettyDate = DateUtils.formatDateTime(c, millis,
-          DateUtils.FORMAT_NUMERIC_DATE);
+      formatFlags = DateUtils.FORMAT_NUMERIC_DATE;
     }
-    return prettyDate;
+    return DateUtils.formatDateTime(c, millis, formatFlags);
   }
 }
